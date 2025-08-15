79675bf05ad823e2408c65b869b608b8e5d1379a
AndresGT <diegogiraldo506@gmail.com>
2025-08-14 21:44:31 -0500

    0.0.1
    se agrega el archivo readme al proyecto

--- obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db70606f4070c4359358cc4e6cf4fe4f80652f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
